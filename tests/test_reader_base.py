dcf4ff8d2a8a1d7a12b5cb39e21652db7f5d1023
Almann Goo <almann@amazon.com>
2016-07-08 12:51:07 -0700

    Adds tests for Blocking Reader.

--- tests/test_reader_base.py
+++ tests/test_reader_base.py
@@ -18,16 +18,20 @@ from __future__ import division
 from __future__ import print_function
 
 from functools import partial
+from io import BytesIO
+
+from pytest import raises
 
 from tests import is_exception, parametrize
-from tests.trampoline_util import always_self, moves_to
+from tests.trampoline_util import always_self
 from tests.trampoline_util import trampoline_scaffold, TrampolineParameters
 
-from amazon.ion.core import DataEvent, Transition
+from amazon.ion.core import Transition
 from amazon.ion.core import ION_VERSION_MARKER_EVENT
 from amazon.ion.core import ION_STREAM_INCOMPLETE_EVENT, ION_STREAM_END_EVENT
 from amazon.ion.reader import read_data_event, reader_trampoline, blocking_reader
 from amazon.ion.reader import NEXT_EVENT, SKIP_EVENT
+from amazon.ion.util import coroutine, record
 
 
 _ivm_transition = partial(Transition, ION_VERSION_MARKER_EVENT)
@@ -102,3 +106,81 @@ _TRIVIAL_DATA_EVENT = read_data_event(b'DATA')
 )
 def test_trampoline(p):
     trampoline_scaffold(reader_trampoline, p)
+
+
+class _P(record('desc', 'coroutine', 'data', 'input', 'expected')):
+    def __str__(self):
+        return self.desc
+
+
+@coroutine
+def _asserts_events(expecteds, outputs):
+    output = None
+    for expected, next_output in zip(expecteds, outputs):
+        actual = yield output
+        assert expected == actual
+        output = next_output
+    yield output
+
+
+@parametrize(
+    _P(
+        desc='ALWAYS COMPLETE',
+        coroutine=_asserts_events(
+            [NEXT_EVENT, SKIP_EVENT],
+            [ION_VERSION_MARKER_EVENT] * 2),
+        data=b'',
+        input=[NEXT_EVENT, SKIP_EVENT],
+        expected=[ION_VERSION_MARKER_EVENT] * 2
+    ),
+    _P(
+        desc='FIRST INCOMPLETE, THEN COMPLETE',
+        coroutine=_asserts_events(
+            [NEXT_EVENT, read_data_event(b'a'), SKIP_EVENT],
+            [ION_STREAM_INCOMPLETE_EVENT] + ([ION_VERSION_MARKER_EVENT] * 2)
+        ),
+        data=b'a',
+        input=[NEXT_EVENT, SKIP_EVENT],
+        expected=[ION_VERSION_MARKER_EVENT] * 2
+    ),
+    _P(
+        desc='FIRST STREAM END, THEN COMPLETE',
+        coroutine=_asserts_events(
+            [NEXT_EVENT, read_data_event(b'a'), SKIP_EVENT],
+            [ION_STREAM_END_EVENT] + ([ION_VERSION_MARKER_EVENT] * 2)
+        ),
+        data=b'a',
+        input=[NEXT_EVENT, SKIP_EVENT],
+        expected=[ION_VERSION_MARKER_EVENT] * 2
+    ),
+    _P(
+        desc='PREMATURE EOF',
+        coroutine=_asserts_events(
+            [NEXT_EVENT, read_data_event(b'a'), read_data_event(b'b')],
+            [ION_STREAM_END_EVENT, ION_STREAM_INCOMPLETE_EVENT, ION_STREAM_INCOMPLETE_EVENT]
+        ),
+        data=b'ab',
+        input=[NEXT_EVENT],
+        expected=[EOFError]
+    ),
+    _P(
+        desc='SINGLE EVENT, THEN NATURAL EOF',
+        coroutine=_asserts_events(
+            [NEXT_EVENT, read_data_event(b'a'), NEXT_EVENT],
+            [ION_STREAM_END_EVENT, ION_VERSION_MARKER_EVENT, ION_STREAM_END_EVENT]
+        ),
+        data=b'a',
+        input=[NEXT_EVENT, NEXT_EVENT],
+        expected=[ION_VERSION_MARKER_EVENT, ION_STREAM_END_EVENT]
+    ),
+)
+def test_blocking_reader(p):
+    buf = BytesIO(p.data)
+    reader = blocking_reader(p.coroutine, buf, buffer_size=1)
+    for input, expected in zip(p.input, p.expected):
+        if is_exception(expected):
+            with raises(expected):
+                reader.send(input)
+        else:
+            actual = reader.send(input)
+            assert expected == actual
