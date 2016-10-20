28733bc297c43f39505bbbbf084ed1341b93fa1b
Michael Montgomery <montgome@amazon.com>
2016-10-20 18:37:08 +0000

    Bumping to post-release version to get around PyPI upload error

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='amazon.ion',
-    version='0.1.0',
+    version='0.1.0post1',
     description='A Python implementation of Amazon Ion.',
     url='http://github.com/amznlabs/ion-python',
     author='Amazon Ion Team',
