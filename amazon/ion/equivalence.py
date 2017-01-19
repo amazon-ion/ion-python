e581d63c713a3905096f9702c068a5090ca5e4d9
Tyler Gregg <greggt@amazon.com>
2017-01-19 12:31:48 -0800

    Completes the equivalence module and adds unit tests.

--- amazon/ion/equivalence.py
+++ amazon/ion/equivalence.py
@@ -26,26 +26,62 @@ from math import isnan
 
 import six
 
-from amazon.ion.core import IonType, _ZERO_DELTA, Timestamp, TimestampPrecision
+from amazon.ion.core import IonType, Timestamp, TimestampPrecision, MICROSECOND_PRECISION, OffsetTZInfo
 from amazon.ion.simple_types import _IonNature, IonPyList, IonPyDict, IonPyTimestamp, IonPyNull, IonPySymbol, \
     IonPyText, IonPyDecimal, IonPyFloat
 from amazon.ion.symbols import SymbolToken
 
 
-def ion_equals(a, b):
-    for a, b in ((a, b), (b, a)):
+def ion_equals(a, b, timestamps_instants_only=False):
+    """Tests two objects for equivalence under the Ion data model.
+
+    There are three important cases:
+        * When neither operand specifies its `ion_type` or `annotations`, this method will only return True when the
+          values of both operands are equivalent under the Ion data model.
+        * When only one of the operands specifies its `ion_type` and `annotations`, this method will only return True
+          when that operand has no annotations and has a value equivalent to the other operand under the Ion data model.
+        * When both operands specify `ion_type` and `annotations`, this method will only return True when the ion_type
+          and annotations of both are the same and their values are equivalent under the Ion data model.
+
+    Note that the order of the operands does not matter.
+
+    Args:
+        a (object): The first operand.
+        b (object): The second operand.
+        timestamps_instants_only (Optional[bool]): False if timestamp objects (datetime and its subclasses) should be
+            compared according to the Ion data model (where the instant, precision, and offset must be equal); True
+            if these objects should be considered equivalent if they simply represent the same instant.
+    """
+    if timestamps_instants_only:
+        return _ion_equals_timestamps_instants(a, b)
+    return _ion_equals_timestamps_data_model(a, b)
+
+
+def _ion_equals_timestamps_instants(a, b):
+    return _ion_equals(a, b, _timestamp_instants_eq, _ion_equals_timestamps_instants)
+
+
+def _ion_equals_timestamps_data_model(a, b):
+    return _ion_equals(a, b, _timestamps_eq, _ion_equals_timestamps_data_model)
+
+
+def _ion_equals(a, b, timestamp_comparison_func, recursive_comparison_func):
+    """Compares a and b according to the description of the ion_equals method."""
+    for a, b in ((a, b), (b, a)):  # Ensures that operand order does not matter.
         if isinstance(a, _IonNature):
             if isinstance(b, _IonNature):
+                # Both operands have _IonNature. Their IonTypes and annotations must be equivalent.
                 eq = a.ion_type is b.ion_type and _annotations_eq(a, b)
             else:
+                # Only one operand has _IonNature. It cannot be equivalent to the other operand if it has annotations.
                 eq = not a.ion_annotations
             if eq:
                 if isinstance(a, IonPyList):
-                    return _sequences_eq(a, b)
+                    return _sequences_eq(a, b, recursive_comparison_func)
                 elif isinstance(a, IonPyDict):
-                    return _structs_eq(a, b)
+                    return _structs_eq(a, b, recursive_comparison_func)
                 elif isinstance(a, IonPyTimestamp):
-                    return _timestamps_eq(a, b)
+                    return timestamp_comparison_func(a, b)
                 elif isinstance(a, IonPyNull):
                     return isinstance(b, IonPyNull) or (b is None and a.ion_type is IonType.NULL)
                 elif isinstance(a, IonPySymbol) or (isinstance(a, IonPyText) and a.ion_type is IonType.SYMBOL):
@@ -57,19 +93,20 @@ def ion_equals(a, b):
                 else:
                     return a == b
             return False
-    # Reaching this point means that neither a nor b has _IonNature.
-    if isinstance(a, list):
-        return _sequences_eq(a, b)
-    elif isinstance(a, dict):
-        return _structs_eq(a, b)
-    elif isinstance(a, datetime):
-        return _timestamps_eq(a, b)
-    elif isinstance(a, SymbolToken):
-        return _symbols_eq(a, b)
-    elif isinstance(a, Decimal):
-        return _decimals_eq(a, b)
-    elif isinstance(a, float):
-        return _floats_eq(a, b)
+    # Reaching this point means that neither operand has _IonNature.
+    for a, b in ((a, b), (b, a)):  # Ensures that operand order does not matter.
+        if isinstance(a, list):
+            return _sequences_eq(a, b, recursive_comparison_func)
+        elif isinstance(a, dict):
+            return _structs_eq(a, b, recursive_comparison_func)
+        elif isinstance(a, datetime):
+            return timestamp_comparison_func(a, b)
+        elif isinstance(a, SymbolToken):
+            return _symbols_eq(a, b)
+        elif isinstance(a, Decimal):
+            return _decimals_eq(a, b)
+        elif isinstance(a, float):
+            return _floats_eq(a, b)
     return a == b
 
 
@@ -77,7 +114,7 @@ def _annotations_eq(a, b):
     return _sequences_eq(a.ion_annotations, b.ion_annotations, _symbols_eq)
 
 
-def _sequences_eq(a, b, comparison_func=ion_equals):
+def _sequences_eq(a, b, comparison_func):
     assert isinstance(a, (list, tuple))
     if not isinstance(b, (list, tuple)):
         return False
@@ -90,7 +127,7 @@ def _sequences_eq(a, b, comparison_func=ion_equals):
     return True
 
 
-def _structs_eq(a, b):
+def _structs_eq(a, b, comparison_func):
     assert isinstance(a, dict)
     if not isinstance(b, dict):
         return False
@@ -105,46 +142,77 @@ def _structs_eq(a, b):
                 key = next(key_iter)
             except StopIteration:
                 break
-            if not ion_equals(a[key], b[key]):
+            if not comparison_func(a[key], b[key]):
                 return False
     return True
 
 
 def _timestamps_eq(a, b):
+    """Compares two timestamp operands for equivalence under the Ion data model."""
     assert isinstance(a, datetime)
     if not isinstance(b, datetime):
         return False
-    _a = a - _ZERO_DELTA
-    _b = b - _ZERO_DELTA
+    # Local offsets must be equivalent.
     if (a.tzinfo is None) ^ (b.tzinfo is None):
         return False
-    if a.tzinfo is not None:
-        assert b.tzinfo is not None
-        _a = a - a.tzinfo.delta
-        _a = _a.replace(tzinfo=None)
-        _b = b - b.tzinfo.delta
-        _b = _b.replace(tzinfo=None)
+    if a.utcoffset() != b.utcoffset():
+        return False
     for a, b in ((a, b), (b, a)):
         if isinstance(a, Timestamp):
             if isinstance(b, Timestamp):
+                # Both operands declare their precisions. They are only equivalent if their precisions are the same.
                 if a.precision is b.precision and a.fractional_precision is b.fractional_precision:
                     break
                 return False
-            elif a.precision is not TimestampPrecision.SECOND:
+            elif a.precision is not TimestampPrecision.SECOND or a.fractional_precision != MICROSECOND_PRECISION:
+                # Only one of the operands declares its precision. It is only equivalent to the other (a naive datetime)
+                # if it has full microseconds precision.
                 return False
-    return _a == _b
+    return a == b
+
+
+def _timestamp_instants_eq(a, b):
+    """Compares two timestamp operands for point-in-time equivalence only."""
+    assert isinstance(a, datetime)
+    if not isinstance(b, datetime):
+        return False
+    # datetime's __eq__ can't compare a None offset and a non-None offset. For these equivalence semantics, a None
+    # offset (unknown local offset) is treated equivalently to a +00:00.
+    if a.tzinfo is None:
+        a = a.replace(tzinfo=OffsetTZInfo())
+    if b.tzinfo is None:
+        b = b.replace(tzinfo=OffsetTZInfo())
+    # datetime's __eq__ implementation compares instants; offsets and precision need not be equal.
+    return a == b
 
 
 def _symbols_eq(a, b):
-    try:
-        a_text = a.text
-    except AttributeError:
-        a_text = a
-    try:
-        b_text = b.text
-    except AttributeError:
-        b_text = b
-    return a_text == b_text
+    assert isinstance(a, (six.text_type, SymbolToken))
+    if not isinstance(b, (six.text_type, SymbolToken)):
+        return False
+    a_text = getattr(a, 'text', a)
+    b_text = getattr(b, 'text', b)
+    if a_text == b_text:
+        if a_text is None:
+            # Both have unknown text. If they come from a local context, they are equivalent.
+            a_location = getattr(a, 'location', None)
+            b_location = getattr(b, 'location', None)
+            if (a_location is None) ^ (b_location is None):
+                return False
+            if a_location is not None:
+                # Both were imported from shared symbol tables. In this case, they are only equivalent if they were
+                # imported from the same position in the same shared symbol table.
+                if (a_location.name != b_location.name) or (a_location.position != b_location.position):
+                    return False
+            a_sid = getattr(a, 'sid', None)
+            b_sid = getattr(b, 'sid', None)
+            if a_sid is None or b_sid is None:
+                raise ValueError('Attempted to compare malformed symbols %s, %s.' % (a, b))
+            if (a_sid == 0) ^ (b_sid == 0):
+                # SID 0 is only equal to SID 0.
+                return False
+        return True
+    return False
 
 
 def _decimals_eq(a, b):
@@ -153,6 +221,7 @@ def _decimals_eq(a, b):
         return False
     if a.is_zero() and b.is_zero():
         if a.is_signed() ^ b.is_signed():
+            # Negative-zero is not equivalent to positive-zero.
             return False
     # This ensures that both have equal precision.
     return a.canonical().compare_total(b.canonical()) == 0
@@ -167,5 +236,7 @@ def _floats_eq(a, b):
     if not isinstance(b, float):
         return False
     if a == 0 and b == 0:
+        # Negative-zero is not equivalent to positive-zero.
         return not (_is_float_negative_zero(a) ^ _is_float_negative_zero(b))
+    # nan is always equivalent to nan.
     return a == b or (isnan(a) and isnan(b))
---
+++ tests/test_equivalence.py
@@ -0,0 +1,382 @@
+# Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at:
+#
+#    http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the
+# License.
+
+"""Tests the `equivalence` module."""
+
+# Python 2/3 compatibility
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from decimal import Decimal
+from functools import partial
+from itertools import chain
+
+from datetime import datetime, timedelta
+
+from amazon.ion.core import IonType, timestamp, TimestampPrecision, OffsetTZInfo
+from amazon.ion.equivalence import ion_equals
+from amazon.ion.simple_types import IonPyNull, IonPyInt, IonPyBool, IonPyFloat, IonPyDecimal, IonPyText, IonPyBytes, \
+    IonPyList, IonPyTimestamp, IonPySymbol, IonPyDict, _IonNature
+from amazon.ion.symbols import SymbolToken, ImportLocation
+from tests import parametrize
+
+
+def _ion_nature(_ion_nature_cls, ion_type, value=None, annotations=()):
+    return _ion_nature_cls.from_value(ion_type, value, annotations)
+
+
+_IT = IonType
+_D = Decimal
+_dt = datetime
+_ts = timestamp
+_TP = TimestampPrecision
+_ST = SymbolToken
+
+_null = partial(_ion_nature, IonPyNull)
+_bool = partial(_ion_nature, IonPyInt, _IT.BOOL)
+_int = partial(_ion_nature, IonPyBool, _IT.INT)
+_float = partial(_ion_nature, IonPyFloat, _IT.FLOAT)
+_decimal = partial(_ion_nature, IonPyDecimal, _IT.DECIMAL)
+_timestamp = partial(_ion_nature, IonPyTimestamp, _IT.TIMESTAMP)
+_string = partial(_ion_nature, IonPyText, _IT.STRING)
+_symbol_text = partial(_ion_nature, IonPyText, _IT.SYMBOL)
+_symbol_token = partial(_ion_nature, IonPySymbol, _IT.SYMBOL)
+_clob = partial(_ion_nature, IonPyBytes, _IT.CLOB)
+_blob = partial(_ion_nature, IonPyBytes, _IT.BLOB)
+_list = partial(_ion_nature, IonPyList, _IT.LIST)
+_sexp = partial(_ion_nature, IonPyList, _IT.SEXP)
+_struct = partial(_ion_nature, IonPyDict, _IT.STRUCT)
+
+
+_EQUIVS = (
+    # For each tuple, all elements in that tuple are equivalent under the Ion data model.
+    (None, _null(_IT.NULL), _null(_IT.NULL)),
+    (True, _bool(True), _bool(1)),
+    (False, _bool(0), _bool(False)),
+    (0, _int(0), _int(0)),
+    (0, -0, _int(0), _int(-0)),
+    (1, _int(1), _int(1)),
+    (-1, _int(-1), _int(-1)),
+    (0.0, 0e0, 0., 0., 0e1, 0e-1),
+    (1.0, 1.0),
+    (float('nan'), float('nan')),
+    (float('+inf'), float('+inf')),
+    (float('-inf'), float('-inf')),
+    (_D(0), _D('0'), _decimal(_D(0)), _decimal(_D('0')), _D('0.'), _decimal(_D('0.'))),
+    (_D('0.0'), _decimal(_D('0.0')), _D('0e-1'), _decimal(_D('0e-1'))),
+    (_D('1.'), _decimal(_D('1e0')), _D('1e-0')),
+    (_D('1e2'), _decimal(_D('1e2'))),
+    (_D('-0.'), _D('-0'), _decimal(_D('-0.')), _decimal(_D('-0'))),
+    (
+        # Regular datetimes are always seconds precision with 6 digits of fractional precision.
+        _dt(1, 1, 1),
+        _dt(1, 1, 1, 0, 0, 0),
+        _timestamp(_dt(1, 1, 1, 0, 0, 0, 0, tzinfo=None)),
+        _ts(1, precision=_TP.SECOND, fractional_precision=6),
+        _timestamp(_ts(1, microsecond=0, precision=_TP.SECOND)),
+    ),
+    (
+        u'abc',
+        _string(u'abc')
+    ),
+    (
+        # Having the same text makes all these equivalent, regardless of symbol ID or import location.
+        u'abc',
+        _symbol_text(u'abc'),
+        _symbol_token(_ST(u'abc', None)),
+        _symbol_token(_ST(u'abc', 10)),
+        _ST(u'abc', 10),
+        _ST(u'abc', None),
+        _ST(u'abc', 10, ImportLocation(u'foo', 1)),
+        _symbol_token(_ST(u'abc', 10, ImportLocation(u'bar', 2)))
+    ),
+    (
+        # Symbols with unknown text from a local symbol table context are equivalent to each other.
+        _symbol_token(_ST(None, 10)),
+        _symbol_token(_ST(None, 11)),
+        _ST(None, 12),
+        _ST(None, 13),
+    ),
+    (
+        _symbol_token(_ST(None, 0)),
+        _ST(None, 0),
+    ),
+    (
+        b'abc',
+        _blob(b'abc'),
+    ),
+    (
+        b'abc',
+        _clob(b'abc'),
+    ),
+    (
+        [],
+        _list([]),
+    ),
+    (
+        [],
+        _sexp([]),
+    ),
+    (
+        {},
+        _struct({}),
+    ),
+    (
+        {u'abc': 123, 'def': 456},
+        _struct({u'def': 456, u'abc': 123}),
+    )
+)
+
+_EQUIVS_INSTANTS = (
+    (
+        # These all represent the same instant.
+        _dt(2000, 1, 1),
+        _dt(2000, 1, 1, tzinfo=(OffsetTZInfo())),
+        _dt(2000, 1, 1, 1, tzinfo=OffsetTZInfo(timedelta(hours=1))),
+        _ts(1999, 12, 31, 23, 59, off_hours=0, off_minutes=-1, precision=_TP.SECOND),
+        _timestamp(_ts(2000, 1, 1, 1, off_hours=1, off_minutes=0, precision=_TP.SECOND)),
+    ),
+)
+
+_NONEQUIVS = (
+    # For each tuple, each element is not equivalent to any other element equivalent under the Ion data model.
+    (None, 0, _null(_IT.BOOL)),
+    (True, False),
+    (True, _bool(False)),
+    (_bool(True), False),
+    (_bool(True), _bool(False)),
+    (1, -1, 1.),
+    (1, _int(-1), _float(1)),
+    (_int(1), _int(-1)),
+    (1, _int(-1)),
+    (1., 1),
+    (-0., 0.),
+    (_float(-0.), _float(0.), _int(0)),
+    (-0., _float(0.), 0, None),
+    (_float(0e1), _float(-0e1)),
+    (float('+inf'), float('-inf')),
+    (_float(float('+inf')), _float(float('-inf'))),
+    (float('+inf'), _float(float('-inf'))),
+    (_float(float('nan')), _float(float('+inf'))),
+    (float('nan'), _float(float('+inf')), None),
+    (_D('-0.'), _D(0), None),
+    (_D('-0'), _decimal(_D(0))),
+    (_decimal(_D('-0')), _decimal(_D('0.'))),
+    (_D('0.'), _D('0e1'), _decimal(_D('0e-1'))),
+    (_D('1.'), _D('1.0'), _decimal(_D('1.00'))),
+    (
+        # Timestamps with different precisions are not equivalent.
+        _dt(1, 1, 1),
+        _timestamp(_ts(1, 1, 1, precision=_TP.DAY)),
+        _ts(1, microsecond=0, precision=_TP.SECOND, fractional_precision=3),
+        _dt(1, 1, 1, tzinfo=OffsetTZInfo()),
+        _timestamp(_ts(1, precision=_TP.SECOND, fractional_precision=6, off_hours=-1, off_minutes=0)),
+        None
+    ),
+    (
+        # Timestamps that represent the same instant with different offsets are not equivalent.
+        _dt(2000, 1, 1),
+        _dt(2000, 1, 1, tzinfo=OffsetTZInfo()),
+        _timestamp(_ts(2000, 1, 1, 1, off_hours=1, off_minutes=0, precision=_TP.SECOND)),
+        _ts(1999, 12, 31, 23, 59, off_hours=0, off_minutes=-1, precision=_TP.SECOND),
+        None,
+    ),
+    (
+        _string(u'abc'),
+        u'abcd',
+        _symbol_text(u'abc'),
+        _symbol_text(u'abcde'),
+        _symbol_token(_ST(None, 10)),
+        _symbol_token(_ST(u'abcdef', 10)),
+        _ST(u'abcdefg', 10),
+        None,
+    ),
+    (
+        _ST(None, 10, ImportLocation(u'foo', 1)),
+        _ST(None, 10, ImportLocation(u'foo', 2)),
+        _symbol_token(_ST(None, 10, ImportLocation(u'bar', 1))),
+        _symbol_token(_ST(None, 10, ImportLocation(u'bar', 2))),
+        _symbol_token(_ST(None, 10)),
+        _ST(None, 0),  # Symbol 0 is only equivalent to itself.
+        None,
+    ),
+    (
+        _blob(b'abc'),
+        _clob(b'abc'),
+        None,
+    ),
+    (
+        _list([]),
+        _sexp([]),
+        {},
+        None
+    ),
+    (
+        _list([]),
+        [u'abc'],
+        [u'def', u'abc'],
+        _list([u'abc', u'def'])
+    ),
+    (
+        _sexp([]),
+        [u'abc'],
+        [u'def', u'abc'],
+        _sexp([u'abc', u'def'])
+    ),
+    (
+        _struct({}),
+        {u'abc': 123}
+    ),
+    (
+        {u'abc': 456, u'def': 123},
+        {u'abc': 123, u'def': 456},
+        _struct({u'abc': 123, u'def': 123}),
+        _struct({u'abc': 456, u'def': 456}),
+    )
+)
+
+
+class _Parameter:
+    def __init__(self, desc, assertion):
+        self.desc = desc
+        self.assertion = assertion
+
+    def __str__(self):
+        return self.desc
+
+_P = _Parameter
+
+
+def _desc(a, b, operator):
+    def _str(val):
+        if isinstance(val, _IonNature):
+            return '%s(%s, ion_type=%s, ion_annotations=%s)' % (type(val), val, val.ion_type, val.ion_annotations)
+        return '%s(%s)' % (type(val), val)
+    return 'assert %s %s %s' % (_str(a), operator, _str(b))
+
+
+def _equivs_param(a, b, timestamp_instants_only=False):
+    def assert_equivalent():
+        assert ion_equals(a, b, timestamp_instants_only)
+        assert ion_equals(b, a, timestamp_instants_only)
+    return _P(_desc(a, b, '=='), assert_equivalent)
+
+
+_equivs_timestamp_instants_param = partial(_equivs_param, timestamp_instants_only=True)
+
+
+def _nonequivs_param(a, b):
+    def assert_not_equivalent():
+        assert not ion_equals(a, b)
+        assert not ion_equals(b, a)
+    return _P(_desc(a, b, '!='), assert_not_equivalent)
+
+
+_TEST_ANNOTATIONS = (
+    (u'abc',),
+    (u'abc', u'def'),
+    (SymbolToken(text=None, sid=10), SymbolToken(text=None, sid=11)),
+)
+
+
+def _generate_annotations():
+    """Circularly generates sequences of test annotations. The annotations sequence yielded from this generator must
+    never be equivalent to the annotations sequence last yielded by this generator.
+    """
+    i = 0
+    while True:
+        yield _TEST_ANNOTATIONS[i]
+        i += 1
+        if i == len(_TEST_ANNOTATIONS):
+            i = 0
+
+_annotations_generator = iter(_generate_annotations())
+
+
+def _add_annotations(val):
+    val.ion_annotations = next(_annotations_generator)
+
+
+def _generate_equivs(equivs, param_func=_equivs_param):
+    for seq in equivs:
+        seq_len = len(seq)
+        for i in range(seq_len):
+            for j in range(seq_len):
+                yield param_func(seq[i], seq[j])
+                # Now, for pairs that are otherwise equivalent, add differing annotation(s) if possible and make sure
+                # they're no longer equivalent.
+                has_ion_nature = False
+                a = seq[i]
+                b = seq[j]
+                if isinstance(a, _IonNature):
+                    has_ion_nature = True
+                    a = a._copy()
+                    _add_annotations(a)
+                if isinstance(b, _IonNature):
+                    has_ion_nature = True
+                    b = b._copy()
+                    _add_annotations(b)
+                if has_ion_nature:
+                    yield _nonequivs_param(a, b)
+
+
+def _generate_nonequivs(nonequivs):
+    for seq in nonequivs:
+        seq_len = len(seq)
+        for i in range(seq_len):
+            for j in range(seq_len):
+                if i != j:
+                    yield _nonequivs_param(seq[i], seq[j])
+
+
+def list_from(equivs, equiv_set_index):
+    output = []
+    for equiv_set in equivs:
+        output.append(equiv_set[equiv_set_index])
+    return output
+
+
+def _generate_equiv_lists(equivs, type_func):
+    a = list_from(equivs, 0)
+    b = list_from(equivs, 1)
+    yield _equivs_param(a, b)
+    yield _equivs_param(a, type_func(b))
+    yield _equivs_param(type_func(a), type_func(b))
+
+
+def _generate_equiv_dicts(equivs):
+    a = list_from(equivs, 0)
+    b = list_from(equivs, 1)
+    assert len(a) == len(b)
+    field_names = [u'%d' % (i,) for i in range(len(a))]
+    a = dict(zip(field_names, a))
+    b = dict(zip(field_names, b))
+    yield _equivs_param(a, b)
+    yield _equivs_param(a, _struct(b))
+    yield _equivs_param(_struct(a), _struct(b))
+
+
+@parametrize(
+    *tuple(chain(
+        _generate_equivs(_EQUIVS),
+        _generate_equivs(_EQUIVS_INSTANTS, _equivs_timestamp_instants_param),
+        _generate_nonequivs(_NONEQUIVS),
+        _generate_equiv_lists(_EQUIVS, _list),
+        _generate_equiv_lists(_EQUIVS, _sexp),
+        _generate_equiv_dicts(_EQUIVS),
+    ))
+)
+def test_equivalence(p):
+    p.assertion()
