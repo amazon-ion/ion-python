df67a8ce387fa133b1ca1bfbaa6248b8a3871f4e
Tyler Gregg <greggt@amazon.com>
2017-05-09 12:39:07 -0700

    Bumps version to 0.1.1.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='amazon.ion',
-    version='0.1.0post1',
+    version='0.1.1',
     description='A Python implementation of Amazon Ion.',
     url='http://github.com/amznlabs/ion-python',
     author='Amazon Ion Team',
