8b1452e60ac20277b23213fe8916d4a8dace5438
Eric Chen <cheqianh@amazon.com>
2022-01-16 13:24:57 -0800

    config

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def run_setup():
 
     setup(
         name='amazon.ion',
-        version='0.9.0',
+        version='0.9.63',
         description='A Python implementation of Amazon Ion.',
         url='http://github.com/amzn/ion-python',
         author='Amazon Ion Team',
@@ -64,7 +64,6 @@ def run_setup():
 
         install_requires=[
             'six',
-            'jsonconversion'
         ],
 
         setup_requires=[
