14ba959b7530143209ee69f22dc7063e2981e4a4
Eric Chen <cheqianh@amazon.com>
2023-05-04 13:53:26 -0700

    test content - pass

--- amazon/ion/core.py
+++ amazon/ion/core.py
@@ -30,7 +30,6 @@ from datetime import datetime, timedelta, tzinfo
 from decimal import Decimal, Context, Inexact
 from math import isnan
 
-
 class IonType(IntEnum):
     """Enumeration of the Ion data types."""
     NULL = 0
