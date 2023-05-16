9c5ce67a0aa8ff303a1857523806c5eef6745d59
Eric Chen <cheqianh@amazon.com>
2023-05-16 13:28:08 -0700

    modify source code to trigger perf detection.

--- amazon/ion/ioncmodule.c
+++ amazon/ion/ioncmodule.c
@@ -201,6 +201,7 @@ static PyObject* ion_build_py_string(ION_STRING* string_value) {
     return PyUnicode_FromStringAndSize((char*)(string_value->value), string_value->length);
 }
 
+
 /*
  *  Adds an element to a List or struct
  *
