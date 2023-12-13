c322eb8b68cf5595a65a7e1a30c2cff7fe39cca4
cheqianh <cheqianh@amazon.com>
2023-12-13 15:37:21 -0800

    change ion benchmarking to iterator so that it's an apple-to-apple comparsion.

--- amazon/ionbenchmark/benchmark_runner.py
+++ amazon/ionbenchmark/benchmark_runner.py
@@ -96,7 +96,6 @@ def _create_test_fun(benchmark_spec: BenchmarkSpec):
     """
     loader_dumper = benchmark_spec.get_loader_dumper()
     match_arg = [benchmark_spec.get_io_type(), benchmark_spec.get_command(), benchmark_spec.get_api()]
-    format_option = benchmark_spec.get_format()
 
     if match_arg == ['buffer', 'read', 'load_dump']:
         with open(benchmark_spec.get_input_file(), 'rb') as f:
@@ -115,12 +114,18 @@ def _create_test_fun(benchmark_spec: BenchmarkSpec):
         data_file = benchmark_spec.get_input_file()
 
         def test_fn():
+            format_option = benchmark_spec.get_format()
             # previously:
             # with open(data_file, "rb") as f:
             #     return loader_dumper.load(f)
             if _format.format_is_ion(format_option):
                 with open(data_file, "rb") as f:
-                    return loader_dumper.load(f)
+                    it = loader_dumper.load(f, parse_eagerly=False)
+                    while True:
+                        try:
+                            next(it)
+                        except StopIteration:
+                            break
             elif _format.format_is_json(format_option):
                 with open(data_file, 'r') as f:
                     for jsonL in f.readlines():
