99aa811e3b8811b3653735f9528b621dbcf2f482
Rob Marrowstone <r.marrowstone@gmail.com>
2024-02-13 16:11:23 -0800

    Remove unused added imports

--- amazon/ion/core.py
+++ amazon/ion/core.py
@@ -11,10 +11,8 @@
 # OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the
 # License.
-import warnings
 from enum import IntEnum
 from typing import NamedTuple, Optional, Any, Union, Sequence, Coroutine
-from warnings import warn
 
 from amazon.ion.symbols import SymbolToken
 
